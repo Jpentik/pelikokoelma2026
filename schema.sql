bdf53bcbb0c4bd1c5e0995b805eda4f9edf7edbf
Jpentik <juha.ao.pentikainen@gmail.com>
2026-08-02 20:39:51 +0300

    Test tables for games and collections added

--- schema.sql
+++ schema.sql
@@ -3,3 +3,17 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE collections (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    sent_at TEXT,
+    user_id INTEGER REFERENCES users,
+    collection_id INTEGER REFERENCES collections
+);
